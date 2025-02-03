72ee16513474b6eb3e0ec8447e06a06142cc2f31
Talha Gergin <talhagergin6@gmail.com>
2025-02-03 12:32:02 +0300

    Initial Commit

---
+++ shopList/shopListApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopListApp.swift
+//  shopList
+//
+//  Created by Talha Gergin on 3.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct shopListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
